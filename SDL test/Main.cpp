deaed011595b24320900de4ff75ba66e27b6de5b
Aitorlb7 <aitor97alb@gmail.com>
2019-02-27 12:17:03 +0100

    SDL Window

--- SDL test/Main.cpp
+++ SDL test/Main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
-int main()
+#include <stdio.h>
+#include "SDL/include/SDL.h"
+#pragma  comment(lib, "SDL/Lib/SDL2main.lib")
+#pragma  comment(lib, "SDL/Lib/SDL2.lib")
+int main(int argc, char* argv[])
 {
-	std::cout << "Hello World" << std::endl;
+	const int SCREEN_WIDTH = 640;
+	const int SCREEN_HEIGHT = 480;
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+	}
+	else
+	{
+		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (window == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+		}
+
+		else
+		{
+		screenSurface = SDL_GetWindowSurface(window);
+		SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+		SDL_UpdateWindowSurface(window);	
+		SDL_Delay(20000);
+		}
+}
+	//Destroy window
+	SDL_DestroyWindow(window);
+
+	//Quit SDL subsystems
+	SDL_Quit();
 	system("pause");
 	return 0;
 }
