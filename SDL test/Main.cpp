f186cdd7cf46df744c84cf4d7b4a5e11fbfa7bf5
paufiol <45202003+paufiol@users.noreply.github.com>
2019-03-06 10:38:15 +0100

    ñe

--- SDL test/Main.cpp
+++ SDL test/Main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-int main()
+#include "SDL/include/SDL.h"
+#pragma comment (lib, "SDL/Lib/SDL2main.lib")
+#pragma comment (lib, "SDL/Lib/SDL2.lib")
+
+int main(int argc, char* argv[])
 {
 	std::cout << "Hello World" << std::endl;
 	system("pause");
