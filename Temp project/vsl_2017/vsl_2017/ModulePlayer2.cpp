1d314423d9a48d2fa0edf1591d17a890124f7a30
paufiol <45202003+paufiol@users.noreply.github.com>
2019-04-28 22:11:16 +0200

    improved win behaviours

--- Temp project/vsl_2017/vsl_2017/ModulePlayer2.cpp
+++ Temp project/vsl_2017/vsl_2017/ModulePlayer2.cpp
@@ -493,6 +493,20 @@ update_status ModulePlayer2::Update()
 		}
 		else App->render->Blit(graphics, position.x + current_animation->GetOffset().x, position.y + current_animation->GetOffset().y, &r, 1.0f, flip);
 	}
+	if (p1Won)
+	{
+		if (wFrame > 0)
+		{
+			//current_animation = &victory;
+			App->render->Blit(graphics2, position.x + current_animation->GetOffset().x, position.y + current_animation->GetOffset().y, &winFrame2, 1.0f, flip);
+		}
+		if (wFrame == 0)
+		{
+			current_animation = &victory;
+			App->render->Blit(graphics2, position.x + current_animation->GetOffset().x, position.y + current_animation->GetOffset().y, &current_animation->GetCurrentFrame(), 1.0f, flip);
+		}
+		wFrame++;
+	}
 	return UPDATE_CONTINUE;
 }
 
